7d9d612dcfcae8d2ac6a43f2b0225f276bd7492e
Surinder Singh Matoo <surinder83singh@gmail.com>
2022-11-23 06:25:34 +0530

    log function renamed

--- src/logs.rs
+++ src/logs.rs
@@ -2,16 +2,16 @@ use wasm_bindgen::prelude::*;
 use workflow_panic_hook::{set_once, Type, show_logs as show_wasm_logs};
 
 #[wasm_bindgen]
-pub fn init_console_logger(){
+pub fn init_console_panic_hook(){
     set_once(Type::Console);
 }
 
 #[wasm_bindgen]
-pub fn init_popup_logger(){
+pub fn init_popup_panic_hook(){
     set_once(Type::Popup);
 }
 
 #[wasm_bindgen]
-pub fn show_logs(){
+pub fn show_panic_hook_logs(){
     show_wasm_logs();
 }
